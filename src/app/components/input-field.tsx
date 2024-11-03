eea55b89983bc94fe0e95aa967cbc21fc1b7df1b
Aleksander <aleksander.0705@gmail.com>
2024-11-03 15:19:53 +0200

    fix type

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React from 'react';
+import React, { ElementType } from 'react';
 import { Field } from 'formik';
 
 export interface InputFieldProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  as?: ElementType;
 }
 
 export default function InputField({ label, id, ...rest }: InputFieldProps) {
